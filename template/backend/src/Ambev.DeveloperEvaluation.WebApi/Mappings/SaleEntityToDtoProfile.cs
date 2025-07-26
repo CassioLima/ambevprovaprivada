2052039736ba2833fe369e3fe38c82f3259e27e0
CassioLima <lima164@gmail.com>
2025-07-26 00:50:09 -0300

    Implementação do Sale

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/SaleEntityToDtoProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Backend.Domain.Entities;
+using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+public class SaleEntityToDtoProfile : Profile
+{
+    public SaleEntityToDtoProfile()
+    {
+        // Mapeia SaleItem -> SaleItemResultDto
+        CreateMap<SaleItem, SaleItemResultDto>();
+
+        // Mapeia Sale -> GetSaleCommandResult
+        CreateMap<Sale, GetSaleCommandResult>()
+            .ForMember(dest => dest.TotalAmount, opt => opt.MapFrom(src => src.TotalAmount))
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items.ToList()));
+    }
+}
